f2e6d224b49daf685d6fddcf1ec832c889c10a7d
MasaGratoR <masagrator@gmail.com>
2023-02-06 20:46:11 +0100

    Add files via upload

--- Compile.py
+++ Compile.py
@@ -4,8 +4,8 @@ import glob
 from pathlib import Path
 import shutil
 
-resx = glob.glob("scn/*.resx.json")
 files = glob.glob("Applied/*.json")
+resx = glob.glob("scn/*.resx.json")
 filtered = []
 
 for i in range(len(resx)):
@@ -20,9 +20,4 @@ os.makedirs("Compiled", exist_ok=True)
 for i in range(len(filtered)):
 	print(Path(filtered[i]).stem)
 	subprocess.run(["psbuild.exe", "-p", "krkr", r"%s" % os.path.abspath(filtered[i])], capture_output=True)
-	file = open("%s.psb" % Path(filtered[i]).stem, "rb")
-	new_file = open("Compiled/%s.txt.scn" % Path(filtered[i]).stem, "wb")
-	new_file.write(file.read())
-	new_file.close()
-	file.close()
-	os.remove("%s.psb" % Path(filtered[i]).stem)
+	shutil.move("%s.psb" % Path(filtered[i]).stem, "Compiled/%s.txt.scn" % Path(filtered[i]).stem)
